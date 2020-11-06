f109cd7777a0545db53d0adb698d1f2f7dbf5c13
LiviaChowdary <68998961+LiviaChowdary@users.noreply.github.com>
2020-11-06 20:11:51 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ var thunderGroup;
 
 
 function preload(){
-  bruce_walking = loadAnimation("walking_1", "walking_2","walking_3","walking_4","walking_5","walking_6","walking_7","walking_8");
+  bruce_walking = loadAnimation("walking_1,png", "walking_2.png","walking_3.png","walking_4.png","walking_5,png","walking_6.png","walking_7.png","walking_8.png");
   
   groundImage = loadImage("ground2.png");
   
